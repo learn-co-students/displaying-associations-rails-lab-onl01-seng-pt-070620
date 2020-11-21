0bc7830b88984039c003b0011cb4b01150739f8a
gracenak <gracenak@gmail.com>
2020-11-21 13:59:43 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+  validate :artist_name
+  validate :index_format
+
+
+  def artist_name
+    self.artist.name
+  end
+
+  def index_format
+    self.artist_name + " - " + self.title
+  end
 end
